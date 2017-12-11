231e699db69a3bcca8290d120ddea78dfb3c1d06
dengzhiyuan <dengzhiyuan@hujiang.com>
2017-12-11 10:24:25 +0800

    提交一版

--- src/main/resources/archetype-resources/__rootArtifactId__-biz/src/main/java/BootLauncher.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-biz/src/main/java/BootLauncher.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author dengzhiyuan
  */
-@SpringBootApplication    //(scanBasePackages = "com.itar.soa.biwan")  这个玩意别瞎加，还好我找到原因了，你自己看下有biwan这个包吗？还scan，我去
+@SpringBootApplication
 public class BootLauncher {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(BootLauncher.class);
