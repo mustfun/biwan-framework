93a8bf75fb170286116a9055cbb177ec657eccfb
dengzhiyuan <dengzhiyuan@hujiang.com>
2018-02-10 17:32:50 +0800

    加入扫描包，不然会因为找不到报错v1

--- src/main/resources/archetype-resources/__rootArtifactId__-biz/src/main/java/biz/BootLauncher.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-biz/src/main/java/biz/BootLauncher.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @ComponentScan(basePackages = {"${package}"})
+@MapperScan("${package}")
 public class BootLauncher {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(BootLauncher.class);
